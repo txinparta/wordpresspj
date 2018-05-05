18ff129a9026dee74eef8f5983e8781f7cc8f1ee
txinparta <36629515+txinparta@users.noreply.github.com>
2018-05-06 00:36:24 +0200

    Update wp-config.php
    Modified: WP_CONTENT_URL. If public/wp-content is included inside a directory, you need to add it to the url.

--- wp-config.php
+++ wp-config.php
@@ -16,7 +16,7 @@ if ( file_exists( dirname( __FILE__ ) . '/../production-config.php' ) ) {
 // Custom Content Directory
 // ========================
 define( 'WP_CONTENT_DIR', dirname( __FILE__ ) . '/wp-content' );
-define( 'WP_CONTENT_URL', 'http://' . $_SERVER['HTTP_HOST'] . '/wp-content' );
+define( 'WP_CONTENT_URL', 'http://' . $_SERVER['HTTP_HOST'] . '/public/wp-content' );
 
 // ================================
 // Language
